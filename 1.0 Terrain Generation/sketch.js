4a95b9d375f9c79ab196a62eea82cc785749f4ed
FMijsters <432601@student.saxion.nl>
2019-01-02 23:19:35 +0100

    terrain generation done

---
+++ 1.0 Terrain Generation/sketch.js
@@ -0,0 +1,46 @@
+let cols, rows;
+const scale = 20;
+const w = 2000;
+const h = 1600;
+let flying = 0;
+var landscape = [];
+
+function setup() {
+    createCanvas(1000, 1000, WEBGL);
+    cols = w / scale;
+    rows = h / scale;
+    landscape = new Array(cols);
+
+}
+
+function draw() {
+
+    flying -= 0.1;
+    let yoff = flying;
+    for (let y = 0; y < rows; y++) {
+        let xoff = 0;
+        let xArray = [rows];
+        for (let x = 0; x < cols; x++) {
+            xArray[x] = map(noise(xoff, yoff), 0, 1, -100, 100);
+            xoff += 0.2;
+        }
+        yoff += 0.2;
+
+        landscape[y] = xArray;
+    }
+
+    background(0);
+    stroke(255);
+    noFill();
+    rotateX(PI / 3);
+    translate(-w / 2, -h / 2 - 100);
+    for (let y = 0; y < rows-1; y++) {
+        beginShape(TRIANGLE_STRIP);
+        for (let x = 0; x < cols; x++) {
+
+            vertex(x * scale, y * scale, landscape[y][x]);
+            vertex(x * scale, (y + 1) * scale, landscape[y + 1][x]);
+        }
+        endShape();
+    }
+}   
---
+++ P5 - Utils/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(600, 600);
+    background(121);
+}
+
+function draw() {
+    
+}   
