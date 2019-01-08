8f3a3ef92a1f23dcbfc5cde846850c2c3bb964ce
FMijsters <432601@student.saxion.nl>
2019-01-08 23:14:22 +0100

    to the point of chord per 0.1 sec

--- 2.0 Sound Analyser/analyser.py
+++ 2.0 Sound Analyser/analyser.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.style as ms
 ms.use('seaborn-muted')
 import sys
+from pychord import Chord
 # and IPython.display for audio output
 import IPython.display
 
@@ -13,6 +14,7 @@ import librosa
 # And the display module for visualization
 import librosa.display
 
+global path
 
 def generateMegSpectogram(y,sr):
 	# Let's make and display a mel-scaled power (energy-squared) spectrogram
@@ -76,26 +78,97 @@ def generateHarmonicPercussiveSourceSeperation(y,sr):
 	plt.tight_layout()
 	plt.show()
 
+def findChord(list_values):
+	index_of_highest = -1
+	highest_value = -1
+	formatter = librosa.display.ChromaFormatter()
+	for i,value in enumerate(list_values):
+		if value > highest_value:
+			index_of_highest = i
+			highest_value = value
+	if highest_value == 0:
+		return "No Chord"
+	return str(formatter.format_data(index_of_highest))
+	
+def clean_map(chords_map):
+	teller= 0
+	teller2 = 0
+	array = list()
+	for key,value in chords_map.items():
+		if value == "No Chord":
+			array.append(key)
+	for value in array:
+		del chords_map[value]
+	return chords_map
+	
+def getMapByChromaOrig(chroma_orig,y,sr):
+	frame_count = chroma_orig.shape[1]
+	total_time= 0
+	chord_by_time_map ={}
+	for i in range(1,frame_count):
+		times = librosa.core.frames_to_time([i-1,i],sr=sr,hop_length=512)
+		total_time = total_time + (times[1] - times[0])
+	rounded_time = round(total_time,2)
+	teller = 0.00
+	time_array = list()
+	while teller != round(rounded_time + 0.01,2):
+		time_array.append(teller)
+		teller = round(teller + 0.01,2)
+	prev_value = 0
+	for i,value in enumerate(time_array):
+		idx = [slice(None), slice(*list(librosa.time_to_frames([prev_value, value])))]
+		if i == 0:
+			continue
+		if chroma_orig[idx].shape[1] == 0:
+			continue
+		chord = findChord(chroma_orig[idx])
+		chord_by_time_map[prev_value] = chord
+		prev_value = value
+
+	return clean_map(chord_by_time_map)
+def checkAllEqualInList(lst):
+	if len(lst) == 0:
+		return False
+	return lst[1:] == lst[:-1]
+
+def summarizeChords(chords_map):
+	teller =0
+	teller2 =0
+	total_array = {}
+	for key,value in chords_map.items():
+		roundend_key = round(key,1)
+		if roundend_key in total_array:
+			array = total_array[roundend_key]
+			array.append(value)
+			total_array[roundend_key] = array
+		else:
+			new_array = list()
+			new_array.append(value)
+			total_array[roundend_key] = new_array
+	cleanList = {}
+	for key,value in total_array.items():
+		if checkAllEqualInList(value):
+			cleanList[key] = value[0]
+	return cleanList
+
 def generateChromagram(y,sr):
+	global path
 	y_harmonic, y_percussive = librosa.effects.hpss(y)
-	# We'll use a CQT-based chromagram here.  An STFT-based implementation also exists in chroma_cqt()
-	# We'll use the harmonic component to avoid pollution from transients
-	chroma_orig = librosa.feature.chroma_cqt(y=y_harmonic, sr=sr)
-	# Make a new figure
-	idx = [slice(None), slice(*list(librosa.time_to_frames([1, 1.1])))]
+	chroma_orig = librosa.feature.chroma_cqt(y=y_harmonic, sr=sr, hop_length=512)
+	
+	chords_map_by_time = getMapByChromaOrig(chroma_orig,y,sr)
+
+	summarized_chords = summarizeChords(chords_map_by_time)
+
+	for key,value in summarized_chords.items():
+		c = Chord(value)
+		c.transpose(-4)
+		print(str(key) + ": " + str(c))
+
+
 
 	plt.figure(figsize=(12,4))
-	# Display the chromagram: the energy in each chromatic pitch class as a function of time
-	# To make sure that the colors span the full range of chroma values, set vmin and vmax
-	librosa.display.specshow(chroma_orig[idx], sr=sr, x_axis='time', y_axis='chroma', vmin=0, vmax=1)
-	# print(C[:,7])
-	# print(C[:,15])
-	# print(C[:,18])
-	# print(C[:,100])
-	formatter = librosa.display.ChromaFormatter()
-	for i,value in enumerate(chroma_orig[[slice(None), slice(*list(librosa.time_to_frames([1, 1.1])))]]):
-		print(str(i) + ": " + str(value) + ": " + str(formatter.format_data(i)))
-	# print(y.shape)
+	librosa.display.specshow(chroma_orig, sr=sr, x_axis='time', y_axis='chroma', vmin=0, vmax=1)
 	plt.title('Chromagram')
 	plt.colorbar()
 
@@ -176,6 +249,8 @@ def generateBeatTracker(y,sr):
 
 
 def analyse(audio_path,analyse_method):
+	global path
+	path = audio_path
 	y, sr = librosa.load(audio_path)
 	print("Succes")
 
@@ -189,12 +264,6 @@ def analyse(audio_path,analyse_method):
 		generateMFCC(y,sr)
 	elif analyse_method == "bt":
 		generateBeatTracker(y,sr)
-
-
-
-
-
-
 
 if __name__ == "__main__":
    if len(sys.argv) < 3:
